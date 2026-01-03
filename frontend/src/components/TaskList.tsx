511cc0b2756cae70eb25b99cc22588443fb7a312
pranavputtagunta <pranav.puttagunta@gmail.com>
2026-01-02 22:39:13 -0600

    Implement part 1.1

---
+++ frontend/src/components/TaskList.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { get } from "src/api/requests";
+import { getAllTasks, type Task } from "src/api/tasks";
+import { TaskItem } from "src/components";
+import styles from "src/components/TaskList.module.css";
+
+export type TaskListProps = {
+  title: string;
+};
+
+type TaskListErrors = {
+  title?: boolean;
+};
+
+export function TaskList({ title }: TaskListProps) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [errors, setErrors] = useState<TaskListErrors>({});
+
+  const [errorModalMessage, setErrorModalMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    // your code here
+    setErrors({});
+    getAllTasks()
+      .then((result) => {
+        if (result.success) {
+          if (setTasks) setTasks(result.data);
+        } else {
+          setErrorModalMessage(result.error);
+        }
+      })
+      .catch(setErrorModalMessage);
+  }, []);
+
+  return (
+    <div className={styles.margin}>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.taskList}>
+        {tasks.length === 0 ? (
+          <p>No tasks yet. Add one above to get started.</p>
+        ) : (
+          tasks.map((task) => <TaskItem key={task._id} task={task} />)
+        )}
+      </div>
+    </div>
+  );
+}
