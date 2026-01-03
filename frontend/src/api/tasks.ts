44d3d611de3101174db41f263d897e26552c5517
pranavputtagunta <pranav.puttagunta@gmail.com>
2026-01-02 22:39:13 -0600

    Implement part 1.1

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -94,3 +94,15 @@ export async function getTask(id: string): Promise<APIResult<Task>> {
     return handleAPIError(error);
   }
 }
+
+export async function getAllTasks(): Promise<APIResult<Task[]>> {
+  try {
+    const response = await get("/api/tasks/");
+    const json = (await response.json()) as TaskJSON[];
+    const tasks = json.map(parseTask);
+    return { success: true, data: tasks };
+    // your code here
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
